9c03ad8db0c830a633d8faec9f53cebc9fabe813
Chintu <neelamdheerajkumar1122@gmail.com>
2024-08-28 16:22:53 +0530

    the

---
+++ add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:22 "
+
+# Ask user for second number
+echo "Enter second number: 44"
+
+# Perform addition
+sum=$((22 + 44))
+
+# Display result
+echo "Sum: $sum"
